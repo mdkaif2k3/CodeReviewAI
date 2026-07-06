a6902cb86bf47754835d3e330554bbddc361f1c8
Mohammed Kaif <mohamkaif2003@gmail.com>
2026-07-06 09:54:40 +0530

    Day 1

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "AI Code Review Assistant API is running",
+  });
+});
+
+module.exports = app;
